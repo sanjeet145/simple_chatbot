190026081c614f4ce43b06576b1211a21ffef8e4
sanjeet145 <sanjeetverma9572@gmail.com>
2025-10-17 00:20:33 +0530

    uvicorn converted to gunicorn

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-gunicorn server:app --bind 0.0.0.0:8000 --workers 4
+python -m gunicorn server:app --bind 0.0.0.0:8000 --workers 4  # its failling in netlify thats why using added python -m
 # uvicorn server:chatapp --reload --host 0.0.0.0 --port 8000 
